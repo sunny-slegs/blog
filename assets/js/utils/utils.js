dc1724b8d02660fe5779b05357570c87fc6709d5
Sonya Slegers <sonjay08@gmail.com>
2019-07-01 21:15:16 -0500

    added utils function skeleton

---
+++ assets/js/utils/utils.js
@@ -0,0 +1,26 @@
+//why is this whole function inside parens with a pair of parens at the end?
+
+const utils = (function() {
+  const extract_params = function(params_string) {
+    console.log("extracting params", params_string);
+    // don't forget to return params
+  };
+
+  return {
+    router: function(route, data) {
+      console.log("router");
+    },
+
+    render: function(element_id, content, convert_JSON) {
+      console.log("render");
+    },
+
+    request: function(
+      api_stub,
+      success_callback,
+      error_callbackcallback_params
+    ) {
+      console.log("request");
+    }
+  };
+})();
